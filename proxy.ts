518f50cf7e1b0f1f1a4352b88d211f784f3a908c
Ahmadsmnaser <Ahmadsmnaser@users.noreply.github.com>
2026-07-09 00:09:29 +0300

    feat: update proxy configuration to include Google site verification file

--- proxy.ts
+++ proxy.ts
@@ -7,6 +7,6 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|googlebc757f548fd4388e.html|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
